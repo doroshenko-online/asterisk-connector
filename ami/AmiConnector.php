9b09d453dc092882ac1ffc7ab9acdebb10ed8653
doroshenko-online <doroshenko@tutanota.com>
2020-11-25 19:28:41 +0200

    создан сокет-клиент для ами и получение его экземпляра

--- ami/AmiConnector.php
+++ ami/AmiConnector.php
@@ -4,12 +4,21 @@
 namespace ami;
 
 
+/**
+ * Class AmiConnector
+ * @package ami
+ */
 class AmiConnector
 {
     private $host;
     private $port;
     private $username;
     private $password;
+    private $config;
+    private $errno;
+    private $errsrt;
+    private $fp = null;
+
     private static $instance = null;
 
     /**
@@ -17,18 +26,57 @@ class AmiConnector
      */
     private function __construct(){}
 
+
     private function init(){
         $this->host = 'localhost';
-        $this->port = '5038';
+//        $this->port = '5038';
+        $this->port = '8423';
         $this->username = 'admin';
         $this->password = 'eLmfSg';
     }
 
-    public static function createConnector(){
+    /**
+     * @return AmiConnector|null
+     */
+    static function createConnector(){
         if(is_null(self::$instance)){
             self::$instance = new self();
         }
-        self::$instance->init();
         return self::$instance;
+    }
+
+    public function getSocket(){
+        if(is_null($this->fp)){
+            $this->init();
+            $this->fp = stream_socket_client($this->host.':'.$this->port, $this->errno, $this->errsrt);
+            if(!$this->fp){
+                throw new \Exception($this->errno.':'.$this->errsrt);
+            }
+        }
+        return $this->fp;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    public function closeConnector(){
+        fwrite($this->fp, "Action: Logoff\r\n\r\n");
+        fclose($this->fp);
+        $this->fp = null;
+        self::$instance = null;
+        print "Соеденение с AMI закрыто\n";
     }
 }
--- main.php
+++ main.php
@@ -2,4 +2,18 @@
 require_once 'ami\AmiConnector.php';
 
 
-\ami\AmiConnector::createConnector();
+$connector = \ami\AmiConnector::createConnector();
+try{
+    $socket = $connector->getSocket();
+}catch (Exception $e){
+    print 'Ami connection error. '.$e;
+    die();
+}
+
+fwrite($socket, "Action: Login\r\n");
+fwrite($socket, "Username: ".$connector->getUsername()."\r\n");
+fwrite($socket, "Secret: ".$connector->getPassword()."\r\n\r\n");
+while (!feof($socket)){
+    $data = fgets($socket, 2048);
+    echo $data;
+}
