624386f5f1359d45dc6729f98510345d8a3b960f
dmitriy.doroshenko <dmitriy.doroshe@cleverty.com.ua>
2021-02-01 18:10:45 +0200

    добавил паралельный http-сокет Workerman, возможность установки хоста и порта в конфиге

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '13ade52ae63f205a169aae5563981a97d4fa0f7b',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '13ade52ae63f205a169aae5563981a97d4fa0f7b',
+    ),
+    'workerman/workerman' => 
+    array (
+      'pretty_version' => 'v4.0.18',
+      'version' => '4.0.18.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '02930876526479c7e4123ef1dc8d23d509d40e72',
+    ),
+  ),
+);
